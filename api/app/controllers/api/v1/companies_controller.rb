a51d647aae0b73950e8e7a1914b60d93957bbc04
MasanoriIwakura <hagobutler@yahoo.co.jp>
2023-10-08 00:58:47 +0900

    feat: add companies

---
+++ api/app/controllers/api/v1/companies_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class CompaniesController < ApplicationController
+      def index
+        @companies = Company.all
+
+        render json: @companies
+      end
+    end
+  end
+end
